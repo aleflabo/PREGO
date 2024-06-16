291076adde8f9c7a08391e12337ce5e672d90994
edodema <edodema13@gmail.com>
2024-06-16 19:14:09 +0200

    Cleaned anticipation
    Cleaned anticipation related code, work has still to be one though.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    data
    	new file:   data/.gitkeep
    	deleted:    launch.sh
    	deleted:    model/log_dist.txt
    	deleted:    model/log_tran&test.txt
    	deleted:    models
    	deleted:    notebooks/SymbolicPatternMachines.ipynb
    	deleted:    notebooks/assembly.ipynb
    	deleted:    notebooks/bending.ipynb
    	deleted:    notebooks/classification_results_converter.ipynb
    	deleted:    notebooks/coin_text_labels.ipynb
    	deleted:    notebooks/davinci.ipynb
    	deleted:    notebooks/emoji.ipynb
    	deleted:    notebooks/general_pattern_machines_ARC.ipynb
    	deleted:    notebooks/general_pattern_machines_PCFG.ipynb
    	deleted:    notebooks/general_pattern_machines_cartpole.ipynb
    	deleted:    notebooks/get_symbolic_data_assembly.ipynb
    	deleted:    notebooks/get_symbolic_data_epic.ipynb
    	deleted:    notebooks/llama.ipynb
    	deleted:    notebooks/llm_dataloader.ipynb
    	deleted:    notebooks/llm_env.yml
    	deleted:    notebooks/metrics_llama.ipynb
    	deleted:    notebooks/new_epic_tents.ipynb
    	deleted:    notebooks/plot_lengths.ipynb
    	deleted:    notebooks/preparing_for_LLAMA.ipynb
    	deleted:    notebooks/preprocessing.ipynb
    	deleted:    notebooks/tokenizer.ipynb
    	deleted:    notebooks/toy_association.ipynb
    	deleted:    run.sh
    	deleted:    run_llama.py
    	deleted:    scripts/lm.sh
    	deleted:    scripts/lstm.sh
    	deleted:    slurm.sh
    	deleted:    src/models/chimera/chimera_old.py
    	deleted:    statistics_Bert.json
    	deleted:    statistics_Bert_baseline.json
    	deleted:    statistics_Bert_finetuned.json

--- scripts/lm.sh
+++
@@ -1,2 +0,0 @@
-PYTHONPATH=. python src/lm.py --lm bert --mask-mode=prob --tokenize-prob 0.25 --epochs 100 --validate-every 10 --wandb-name bert_mask_prob --wandb-group lm --wandb-mode disabled
-PYTHONPATH=. python src/lm.py --lm bert --mask-mode=end --epochs 100 --validate-every 10 --wandb-name bert_mask_end --wandb-group lm --wandb-mode disabled
--- scripts/lstm.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-PYTHONPATH=. python src/run.py --cfg/lstm.yaml --wandb-group 'forecasting' --wandb-name 'lstm' --wandb-tags 'train' --wandb-mode 'disabled'
